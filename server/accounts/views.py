bcacc2b766ca9c634ee53e78903a641d126995c9
Azazul Islam <200118.cse@student.just.edu.bd>
2026-04-01 01:50:25 +0600

    some modificatin for configuration superuser

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 
